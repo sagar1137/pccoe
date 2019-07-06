1c7a98d7b78da3a4c3e18a9641c054e66c621d36
sagar1137 <patilsagar1535@gmail.com>
2019-07-06 16:07:06 +0530

    Initial commit

---
+++ src/main/java/com/pccoe/pccoe/PccoeApplication.java
@@ -0,0 +1,13 @@
+package com.pccoe.pccoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PccoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PccoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pccoe/pccoe/PccoeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pccoe.pccoe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PccoeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
